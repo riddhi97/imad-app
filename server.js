e706df9109c7e5b2b863d363d993725703c2cfb5
riddhi97 <31026825+riddhi97@users.noreply.github.com>
2017-09-20 10:26:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send('Artcle one request will be sent');
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two', function (req, res) {
